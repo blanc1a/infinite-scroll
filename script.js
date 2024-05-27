acf57802fb33c6c1b72d95874981f11baf17653e
Blanca Ziems <blanca481@hotmail.de>
2024-05-27 20:49:31 +0200

    add: more code

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const imgContainer = document.getElementById("img-container");
+const loader = document.getElementById("loader");
+
+let photosArray = []; //state
+
 //Unsplash API
 //We can create an App/a Project => get API-Key
 //https://unsplash.com/documentation#get-a-random-photo
@@ -12,10 +17,47 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
+    const photosArray = await response.json();
+    console.log(photosArray);
+    renderPhotos();
   } catch (error) {
     //catch error here
   }
+}
+
+//Helper Function to set attributes on DOM elements
+//=> to have dry code => which means that the codes does not repeat itself unnecessarily
+function setAttributes(element, attributes) {
+  //key is gonna be the href, src, alt, title
+  //attributes is gonna be the obj which conaints both the key and the value we actually wanna set
+  for (const key in attributes) {
+    //element is gonna be item or img
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function renderPhotos(photosArray) {
+  //photos is a self named el in the array => just like we do in the for in or for of loops
+  photosArray.forEach((photo) => {
+    //create link to unsplash
+    const item = document.createElement("a");
+    //Go to dev tools =>fetch request => show preview to see which attributes to add
+    //in this case each obj has the nested property links which includes a html link (link back to the unsplash site)
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    //create img for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    //append the elements
+    item.appendChild(img);
+    imgContainer.appendChild(item);
+  });
 }
 
 //On load
